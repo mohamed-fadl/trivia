9f7d219d4a500d9e2fbe5f5b9dc45c98069a9bf2
Mohamed Elkhidir <moel@sto-m-moel.local>
2018-09-26 21:06:19 +0200

    added basic data models and simple logic

---
+++ typescript/src/data-models/game/GameBoard.ts
@@ -0,0 +1,46 @@
+/* this is a representation of the game board, it can have different shapes but
+    for the current purpose of this game it reprepsents a one dimensional array of 12 element
+    that looks like this
+
+    *****************************************************************************************************************************
+    *  0 pop * 1 science * 2 sports * 3 rock  * 4 pop * 5 science * 6 sports * 7 rock * 8 pop * 9 science * 10 sports * 11 rock *
+    *****************************************************************************************************************************
+ 
+*/
+
+export class GameBoard {
+
+    boardArray: Array<string>;
+    callsCount: any // save number of calls for each category
+
+    constructor() {
+        this.boardArray = new Array<string>();
+        this.callsCount = new Map();
+    }
+
+    initalizeTheBoard() {
+        this.boardArray = ['pop', 'science', 'sports', 'rock', 'pop', 'science', 'sports', 'rock', 'pop', 'science', 'sports', 'rock'];
+
+        this.callsCount.set('pop', 0);
+        this.callsCount.set('science', 0);
+        this.callsCount.set('rock', 0);
+        this.callsCount.set('sports', 0);
+
+    }
+
+    getCategory(index): string {
+
+        var category = this.boardArray[index];
+        this.updateCategoryCalls(category);
+
+        return category;
+    }
+
+    updateCategoryCalls(category) {
+        //this.callsCount.set(category, this.callsCount.get(category) + 1);
+    }
+
+    getCategoryCalls(category): number {
+        return this.callsCount.get(category)
+    }
+}
---
+++ typescript/src/data-models/game/questionsGenerator.ts
@@ -0,0 +1,30 @@
+// this module generates questions for the game players.
+
+import { GameBoard } from './GameBoard';
+
+export class QuestionsGenerator {
+
+    private gameBoard: GameBoard;
+
+    constructor(gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+
+
+    generateQuestion(playerPosition): string {
+        var result: string;
+
+        // check the category from the GameBoard
+        var questionCategory = this.gameBoard.getCategory(playerPosition);
+        var categoryCalls = this.gameBoard.getCategoryCalls(questionCategory);
+
+
+        result = this.formatQuestion(questionCategory, categoryCalls);
+        return result;
+    }
+
+    formatQuestion(category, callsCount): string {
+
+        return `${category} Question ${callsCount}`;
+    }
+}
