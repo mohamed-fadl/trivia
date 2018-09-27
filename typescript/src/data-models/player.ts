fd178791553719d7e271e30166e5359b3eefdfa8
mhharktat@gmail.com <>
2018-09-27 16:47:18 +0200

    added playerPosition to the player class

--- typescript/src/data-models/player.ts
+++ typescript/src/data-models/player.ts
@@ -2,7 +2,8 @@ export class Player {
     public name: string;
     public coins: number = 0;
     public isPenalized: boolean = false;
-
+    public positionInTheGameBoard :number = 0;
+    
     constructor(name: string) {
         this.name = name;
 
