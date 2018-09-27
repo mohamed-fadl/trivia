17099edad0550bb80b239bc98c2286e7eb338a4c
mhharktat@gmail.com <>
2018-09-27 16:51:07 +0200

    the judge starts the game by combining the game, players and applying the rules

--- typescript/src/data-models/game/judge.ts
+++ typescript/src/data-models/game/judge.ts
@@ -1,8 +1,11 @@
 import { Player } from './../player';
+import {Game} from './game'
 // this module to judge a game state if it's finished with a winner or not 
 
 export class Judge {
 
+    constructor(private game:Game, private players:Array<Player>){
+    }
     checkGameForWinner(player: Player): boolean {
 
         if (player.coins == 6) {
@@ -10,5 +13,35 @@ export class Judge {
         }
 
         return false;
+    }
+
+    public startTheGame(){
+        // this function implements the logic of how the game is played
+        
+        /*  
+        circulate between players, roll the dice for each of them to know which questions
+        category they should answer a question from. Ask the question and check if the answer
+        is correct and reward or penalize the players according to that 
+        */
+
+        var i = 0;
+        do{
+            var currentPlayer = this.players[i];
+            var positionShift = this.rollTheDice();
+
+            // move the player x positions on the game board
+            currentPlayer.positionInTheGameBoard =  this.game.movePlayerInTheBoard(currentPlayer.positionInTheGameBoard 
+                                                                                    + positionShift);
+            
+            // get the question 
+            this.
+
+        }while(i != -1)
+
+    }
+
+
+    private rollTheDice():number{
+        return Math.floor(Math.random() * 6) + 1
     }
 }
