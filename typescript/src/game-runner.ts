df89581bb79e05816f270532478701e5a9fc58fb
mhharktat@gmail.com <>
2018-09-28 10:42:57 +0200

    added the new way of playing the game to the game runner and removed the old code

--- typescript/src/game-runner.ts
+++ typescript/src/game-runner.ts
@@ -4,32 +4,11 @@ import { Judge } from './data-models/game/judge';
 
 export class GameRunner {
     public static main(): void {
-        // const game = new Game();
-        // game.add("Chet");
-        // game.add("Pat");
-        // game.add("Sue");
-
-        // let notAWinner;
-        // do {
-
-        //     game.roll(Math.floor(Math.random() * 6) + 1);
-
-        //     if (Math.floor(Math.random() * 10) == 7) {
-        //     notAWinner = game.wrongAnswer();
-        //     } else {
-        //     notAWinner = game.wasCorrectlyAnswered();
-        //     }
-
-        // } while (notAWinner);
-
-        // the game runner starts by creating the players, the game, and the judge
-        // the game runner then lets the judge manage the game and the players and decide
-        // on the game resutl.
 
         // create list of players
         var playersFactory = new PlayersFactory();
         var players = playersFactory.createPlayers();
-
+        
         // create the game
         var theGame = new Game();
 
